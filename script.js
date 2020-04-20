9ed2508ff230c581f44c2db924011c3000decdf3
Sven Henrik Karlsson <henrik@macbook-pro-16.lan>
2020-04-20 23:44:45 +0200

    shorter uuid

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ if (isIE) {
 }
 
 function uuidv4() {
-	return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+	return ([1e7]).replace(/[018]/g, c =>
 		(c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
 	)
 }
@@ -15,7 +15,6 @@ if (document.cookie.indexOf('uuid=') == -1) {
 }
 
 var uuid = document.cookie.split('=')[1]
-
 
 var globalValues, clickArea, parkeringar, aktivParkering, referefenceMidpoints, scaler, categoryColumns
 var shownFIDs = []
