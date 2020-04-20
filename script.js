f059d2bc3efdf177662f32f3ba1c2b0ea4cf1b45
Sven Henrik Karlsson <henrik@macbook-pro-16.lan>
2020-04-20 23:27:08 +0200

    new uuid

--- script.js
+++ script.js
@@ -10,6 +10,16 @@ function uuidv4() {
 	)
 }
 
+function uuidv4() {
+	// I generate the UID from two parts here 
+	// to ensure the random number provide enough bits.
+	var firstPart = (Math.random() * 46656) | 0;
+	var secondPart = (Math.random() * 46656) | 0;
+	firstPart = ("000" + firstPart.toString(36)).slice(-3);
+	secondPart = ("000" + secondPart.toString(36)).slice(-3);
+	return firstPart + secondPart;
+}
+
 document.cookie.indexOf('uuid=')
 console.log(document.cookie)
 
