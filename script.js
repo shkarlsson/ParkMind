17c326767bee157d90d951c72989e4104f4cf852
Sven Henrik Karlsson <henrik@macbook-pro-16.lan>
2020-04-20 23:48:52 +0200

    tostring 35->36

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ if (isIE) {
 
 function uuidv4() {
 	return ([1e7] + 1e3).replace(/[018]/g, c =>
-		(c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(32)
+		(c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 35 >> c / 4).toString(36)
 	)
 }
 
