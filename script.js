3e8998dc9c8bdae04b5aa62dff780a6288a638e0
Sven Henrik Karlsson <henrik@macbook-pro-16.lan>
2020-04-20 23:56:10 +0200

    evenshorter

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ if (isIE) {
 }
 
 function uuidv4() {
-	return ([1e7] + 1e3).replace(/[018]/g, c =>
-		(c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 35 >> c / 4).toString(36)
+	return (1e7).replace(/[018]/g, c =>
+		(c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
 	)
 }
 
